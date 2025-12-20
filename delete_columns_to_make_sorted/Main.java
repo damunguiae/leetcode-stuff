7f4e5b7885aa1dd980c11548c646dbea3ff63325
damunguiae <damunguia@proton.me>
2025-12-20 14:21:05 -0600

    Refactor minDeletionSize method for improved readability and consistency

--- delete_columns_to_make_sorted/Main.java
+++ delete_columns_to_make_sorted/Main.java
@@ -9,25 +9,33 @@ public class Main {
 
 public static int minDeletionSize(String[] strs) {
         //get matrix dimensions
-        Integer dimensions = strs[0].length();
+        int dimensions = strs[0].length();
         //System.out.println("dimension: " + dimensions);
         int errors = 0;
+        char initialColumn = strs[0].charAt(0);
+        if (initialColumn == 'z') {
+            return strs.length;
+
+        }
 
         for (int j = 0; j < dimensions; j++) {
             char column = strs[0].charAt(j);
+            if (column == 'z') {
+                errors++;
+                continue;
+            }
             //System.out.println("column: " + column);
             for (int i = 1; i < strs.length; i++) {
                 //System.out.println("comparing: " + column + " and: "+strs[i].charAt(j));
                 char currentChar = strs[i].charAt(j);
-                 if(column>currentChar)
-                 {
+                if (column > currentChar) {
                     //System.out.println("error");
                     errors++;
                     column = strs[0].charAt(j);
                     break;
-                 }else{
-                    column=currentChar;
-                 }
+                } else {
+                    column = currentChar;
+                }
 
             }
 
